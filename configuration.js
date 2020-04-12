9cc719c8c532acbf7ce970b4ed80451239f90e1a
Irene Olmos <ireneog_72@hotmail.es>
2020-04-12 23:11:04 +0200

    Initial commit

---
+++ configuration.js
@@ -0,0 +1,5 @@
+exports.config = {
+  // launch locally when fields directConnect and seleniumAddress are not provided
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec1.js']
+  };
